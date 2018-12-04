c0fb6e9249396185918b18501ad1401f56bd2673
trucomanx <fernando.pujaico.rivera@gmail.com>
2018-12-04 15:01:11 -0200

    ordenando arquivos

--- 5var/principal.m
+++ 5var/principal.m
@@ -1,5 +1,8 @@
+%%%
 clc
 MINIMO_ERROR = 0.1;
+
+addpath(genpath(mcode))
 
 format long
 page_screen_output(0);
@@ -7,8 +10,8 @@ page_output_immediately(1);
 
 alfa=1.9;
 
-d0=[  -159, -159,  -159,  -159 ];
-c0=[ 16    ,  8  ,    24,    43  ];
+d0=[-159   ,-159 ,  -159,   -159];
+c0=[ 16    ,  8  ,    24,     43];
 C =[4.5    ;   2 ;   6.5;   12.3];
 h0=246.1523; D=23; Q= 1.3123; f=15; g=109;
 X0=[h0;D;Q;f;g];
