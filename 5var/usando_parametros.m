f8a9a28f8ac3b894db98adec0aea0240f31407dd
trucomanx <fernando.pujaico.rivera@gmail.com>
2018-12-13 15:40:41 -0200

    ordenando

--- 5var/usando_parametros.m
+++ 5var/usando_parametros.m
@@ -1,31 +1,31 @@
 %%
-clc
 close all
-addpath(genpath('mcode'))
 page_screen_output(0);
 page_output_immediately(1);
-OUTPUT='output';
 
-IMAGEPATH='/home/fernando/MapaDePerfil/povray/line-laser-ProjA-CamA/output/8bit/main1.bmp';
-IMG = imread(IMAGEPATH);
-IMG_BIN=IMG>0.5;
+addpath(genpath('mcode'))
+OUTPUT='output';
+mkdir(OUTPUT);
 
 
 IMAGEPATH='/home/fernando/MapaDePerfil/povray/line-laser-ProjA-CamA/output/8bit/main0.bmp';
 IMG = imread(IMAGEPATH);
 IMG_BIN_REF=IMG>0.5;
 
-h0=400; 
-D=1.0; 
-Q= 60*pi/180; 
-f=1.8; 
-g=1.8;
+IMAGEPATH='/home/fernando/MapaDePerfil/povray/line-laser-ProjA-CamA/output/8bit/main1.bmp';
+IMG = imread(IMAGEPATH);
+IMG_BIN=IMG>0.5;
 
-PARAMS=[h0;D;Q;f;g];
+
+PARAMS=[    h0=400; ...
+            D=1.0; ...
+            Q=pi/3; ...
+            f=1.8; ...
+            g=1.8];
 
 [B Y C]=image1linefun(PARAMS,IMG_BIN,IMG_BIN_REF,false,OUTPUT);
 
-mkdir(OUTPUT);
+
 figure
 plot(B,C,'-o');
 grid minor on
